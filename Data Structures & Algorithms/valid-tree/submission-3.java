1a25bf3bbb8f71a77c6b07310961010548623c14
Anand Rai <anand.it402@gmail.com>
2026-06-04 23:47:30 +0530

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if(edges.length != n-1) return false;
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<edges.length; i++) { 
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        Set<Integer> visited = new HashSet<>();
+        Set<Integer> currentPath = new HashSet<>();
+
+        for(int i =0; i<n; i++) {
+            if(hasCycle(i, -1, visited, currentPath, adj)) return false;
+        }
+
+        return visited.size() == n;
+    }
+
+    private boolean hasCycle(int node, int parent, Set<Integer> visited, Set<Integer> currentPath, List<List<Integer>> adj) {
+        if(visited.contains(node)) return false;
+        if(currentPath.contains(node)) return true;
+
+        currentPath.add(node);
+
+        for(int n : adj.get(node)) {
+            if(n == parent) continue;
+            if(hasCycle(n, node, visited, currentPath, adj)) return true;
+        }
+
+        visited.add(node);
+        currentPath.remove(node);
+
+        return false;
+    }
+}
